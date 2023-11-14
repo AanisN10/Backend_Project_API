2bbb65d83608176bf40f9d2279390c7ed9f33e87
Aanis Nuur <Aanisn10@gmail.com>
2023-11-14 14:28:08 +0000

    added @GetMapping by id -created method for GetquizById - tested on postman

--- WhoAreYou/src/main/java/com/quiz/WhoAreYou/controllers/QuizController.java
+++ WhoAreYou/src/main/java/com/quiz/WhoAreYou/controllers/QuizController.java
@@ -1,15 +1,18 @@
 package com.quiz.WhoAreYou.controllers;
 
+import com.quiz.WhoAreYou.models.Question;
 import com.quiz.WhoAreYou.models.Quiz;
 import com.quiz.WhoAreYou.services.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/quizzes")
@@ -21,4 +24,17 @@ public class QuizController {
     public ResponseEntity<List<Quiz>> getAllQuizzes(){
         return new ResponseEntity<>(quizService.findAllQuizzes(), HttpStatus.OK);
     }
+
+    @GetMapping (value = "/{id}")
+
+    public ResponseEntity<Quiz> getQuizById(@PathVariable Long id){
+
+        Optional<Quiz> getQuizById = quizService.findQuizById(id);
+        if(getQuizById.isPresent()){
+            return new ResponseEntity<>(getQuizById.get(), HttpStatus.OK);
+        } else{
+            return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
--- WhoAreYou/src/main/java/com/quiz/WhoAreYou/services/QuizService.java
+++ WhoAreYou/src/main/java/com/quiz/WhoAreYou/services/QuizService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class QuizService {
 
@@ -14,5 +16,9 @@ public class QuizService {
 
     public List<Quiz> findAllQuizzes() {
         return quizRepository.findAll();
+    }
+
+    public Optional<Quiz> findQuizById(Long id) {
+        return quizRepository.findById(id);
     }
 }
