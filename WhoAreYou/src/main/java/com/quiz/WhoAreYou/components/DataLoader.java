5c7b20b43f9559543307ad1c76eadaf86525e463
Suzi Towers <suzitowers@gmail.com>
2023-11-15 11:56:24 +0000

    added questions to the dataloader

--- WhoAreYou/src/main/java/com/quiz/WhoAreYou/components/DataLoader.java
+++ WhoAreYou/src/main/java/com/quiz/WhoAreYou/components/DataLoader.java
@@ -26,21 +26,128 @@ public class DataLoader implements ApplicationRunner {
     public void run (ApplicationArguments args)throws Exception{
 
         Question question1 = new Question(
-                "What is your favourite colour",
-                "Purple",
-                "Blue",
-                "Red",
-                "Green",
-                "C","D",
-                "A","B");
+                "If you could be a biscuit, which one would you be and why?",
+                "Hobnob (not chocolate, original only!)",
+                "Ginger Nut - A good all-rounder",
+                "Viennese w/Chocolate - I have no reason, I was just eating them the day before",
+                "Wagon wheels - crunchy on the outside, soft on the inside",
+                "D",
+                "A",
+                "B",
+                "C");
+
+        Question question2 = new Question(
+                "If you ever got arrested, what would it most likely be for?",
+                "Shouting at teenagers for littering (it’s a rite of passage for mums)",
+                "Doing 22mph in a 20 zone",
+                "I wouldn’t get arrested - I’d get away with whatever crime I commit :)",
+                "Being obnoxiously loud or breaking spaghetti in half (the Italian police are ruthless",
+                "D",
+                "A",
+                "B",
+                "C");
+
+        Question question3 = new Question(
+                "What is your favourite chocolate?",
+                "Kinder Bueno",
+                "Wispa",
+                "Ferrero Rocher",
+                "Bounty, other answers, while respected, are factually wrong",
+                "D",
+                "A",
+                "B",
+                "C");
+
+        Question question4 = new Question(
+                "Imagine you can solve any problem with code. What is the first problem you’d like to solve?",
+                "School websites",
+                "Perfecting the milk:tea ratio",
+                "Access to education for all children around the world",
+                "Automating my cooking",
+                "D",
+                "A",
+                "B",
+                "C");
+
+        Question question5 = new Question(
+                "What is your dream job?",
+                "This one!",
+                "Luthier",
+                "A cafe owning florist",
+                "Teaching a bunch of rockstars",
+                "D",
+                "A",
+                "B",
+                "C");
+
+        Question question6 = new Question(
+                "What is your favourite genre of music?",
+                "Folk/Country",
+                "Blues",
+                "I don’t have a favourite, it depends on my mood - hip hop?",
+                "Metal",
+                "D",
+                "A",
+                "B",
+                "C");
+
+        Question question7= new Question(
+                "What is your spirit vegetable and why?",
+                "Carrots, anything to improve my eyesight",
+                "Cauliflower - affinity for cheese sauce",
+                "Potatoes - I used to always wear really baggy clothes",
+                "San Marzano tomato",
+                "D",
+                "A",
+                "B",
+                "C");
+
+        Question question8 = new Question(
+                "Where is your ideal holiday destination and why?",
+                "France or Singapore - for the food",
+                "A small town in Italy - peaceful, good weather and excellent food",
+                "Jeju Island - peaceful and surrounded by water",
+                "Perugia, Italy - amazing food, good coffee, lovely people, beautiful countryside",
+                "D",
+                "A",
+                "B",
+                "C");
+
+        Question question9 = new Question(
+                "What is the best bean?",
+                "Chickpea",
+                "Kidney",
+                "@Repository",
+                "A good arabica dark roast",
+                "D",
+                "A",
+                "B",
+                "C");
+
 
         questionRepository.save(question1);
+        questionRepository.save(question2);
+        questionRepository.save(question3);
+        questionRepository.save(question4);
+        questionRepository.save(question5);
+        questionRepository.save(question6);
+        questionRepository.save(question7);
+        questionRepository.save(question8);
+        questionRepository.save(question9);
 
-        Quiz quiz1 = new Quiz(false,0,0,0,0,1);
+        Quiz quiz1 = new Quiz(false,0,0,0,0,9);
 
         quizRepository.save(quiz1);
 
         quiz1.addQuestion(question1);
+        quiz1.addQuestion(question2);
+        quiz1.addQuestion(question3);
+        quiz1.addQuestion(question4);
+        quiz1.addQuestion(question5);
+        quiz1.addQuestion(question6);
+        quiz1.addQuestion(question7);
+        quiz1.addQuestion(question8);
+        quiz1.addQuestion(question9);
 
         quizRepository.save(quiz1);
     }
